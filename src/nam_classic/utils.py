1401cd8210f2bf321a6bd5a433931af159e81171
Rasmus Munkner <reu@topdanmark.dk>
2026-01-23 13:10:12 +0100

    Deleted old files from repo.

--- src/nam_classic/utils.py
+++
@@ -1,4 +0,0 @@
-from jax import numpy as jnp
-
-def condition(saturation: jnp.ndarray, threshold: jnp.ndarray) -> jnp.ndarray:
-    return (saturation - threshold) / (1 - threshold) * (saturation > threshold)
